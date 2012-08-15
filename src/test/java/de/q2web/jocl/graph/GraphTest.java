823c24de7d3672f6b2dbd7ab4987fa898dcff5d2
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-08-15 16:08:06 +0200

    Add bi-directional tests

--- src/test/java/de/q2web/jocl/graph/GraphTest.java
+++ src/test/java/de/q2web/jocl/graph/GraphTest.java
@@ -1,64 +1,56 @@
-/*
- *******************************************************************************
- * GraphTest.java
- * $Id: $
- *
- *******************************************************************************
- *
- * Copyright:   Q2WEB GmbH
- *              quality to the web
- *
- *              Tel  : +49 (0) 211 / 159694-00	Kronprinzenstr. 82-84
- *              Fax  : +49 (0) 211 / 159694-09	40217 Düsseldorf
- *              eMail: info@q2web.de						http://www.q2web.de
- *
- *
- * Author:      oliver.schrenk
- *
- * Created:     Aug 13, 2012
- *
- * Copyright (c) 2009 Q2WEB GmbH.
- * All rights reserved.
- *
- *******************************************************************************
- */
 package de.q2web.jocl.graph;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
 /**
- * 
- * @author oliver.schrenk
- * @version
- *          $Id: $
+ *
+ * @author Oliver Schrenk <oliver.schrenk@q2web.de>
+ * @version $Id: $
  */
 public class GraphTest {
 
-	private static final int A=0;
-	private static final int B=1;
-	private static final int C=2;
-	private static final int D=3;
+	// vertices
+	private static final int A = 0;
+	private static final int B = 1;
+	private static final int C = 2;
+	private static final int D = 3;
 
-	private static final int DEFAULT_WEIGHT=1;
+	private static final int DEFAULT_WEIGHT = 1;
 
 	@Test
-	public void test() {
-		final int[] expectedVertexList={
-		                                0, 2, 3
-		};
-		int[] expectedEdgeList={
-		                        1, 2, 3, 3
-		};
-		int[] expectedWeightList={
-		                          DEFAULT_WEIGHT, DEFAULT_WEIGHT, DEFAULT_WEIGHT, DEFAULT_WEIGHT
-		};
-		Graph graph=new Graph(3, 4);
+	public void testUniDirectional() {
+		final int[] expectedVertexList = { 0, 2, 3 };
+		final int[] expectedEdgeList = { 1, 2, 3, 3 };
+		final int[] expectedWeightList = { DEFAULT_WEIGHT, DEFAULT_WEIGHT,
+				DEFAULT_WEIGHT, DEFAULT_WEIGHT };
+
+		final Graph graph = new Graph(3, 4);
 		graph.addEdge(A, B, DEFAULT_WEIGHT);
 		graph.addEdge(A, C, DEFAULT_WEIGHT);
 		graph.addEdge(B, D, DEFAULT_WEIGHT);
 		graph.addEdge(C, D, DEFAULT_WEIGHT);
+
+		assertArrayEquals(expectedVertexList, graph.getVertexArray());
+		assertArrayEquals(expectedEdgeList, graph.getEdgeArray());
+		assertArrayEquals(expectedWeightList, graph.getWeightArray());
+	}
+
+	@Test
+	public void testBiDirectional() {
+		final int[] expectedVertexList = { 0, 2, 4 };
+		final int[] expectedEdgeList = { 1, 2, 2, 3, 0, 1, 3 };
+		final int[] expectedWeightList = { 4, 2, 3, 1, 2, 1, 5 };
+
+		final Graph graph = new Graph(3, 7);
+		graph.addEdge(A, B, 4);
+		graph.addEdge(A, C, 2);
+		graph.addEdge(B, C, 3);
+		graph.addEdge(B, D, 1);
+		graph.addEdge(C, A, 2);
+		graph.addEdge(C, B, 1);
+		graph.addEdge(C, D, 5);
 
 		assertArrayEquals(expectedVertexList, graph.getVertexArray());
 		assertArrayEquals(expectedEdgeList, graph.getEdgeArray());
